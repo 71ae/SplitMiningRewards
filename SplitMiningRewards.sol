bca216a2914308a7e780ea5ad520c82e78d0dc24
Andreas E. (71ae) <47578577+71ae@users.noreply.github.com>
2020-03-12 18:45:13 +0100

    preparation for chaning payee addresses (#15)

--- SplitMiningRewards.sol
+++ SplitMiningRewards.sol
@@ -113,7 +113,18 @@ contract SplitMiningRewards
         }
     }
 
-    // Maintenance: Change payee addresses
+    // Maintenance: Change costcenter payee address
+    function changePayeeCostcenter(address _newCostcenter) public onlyDelegates()
+    {
+
+    }
+
+    // Maintenance: Change beneficiary payee address
+    function changePayeeBeneficiary(address _newBeneficiary) public onlyDelegates()
+    {
+
+    }
+
     // TO-DO: Write some more functions.
 
 }
